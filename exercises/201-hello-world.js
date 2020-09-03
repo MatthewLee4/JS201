1132adfde2f20537084d29f8841e592b354a3711
MatthewLee4 <67612225+MatthewLee4@users.noreply.github.com>
2020-09-03 17:51:53 -0500

    Updated the README.md file

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -5,8 +5,16 @@
 // - hello('banana') should return 'Hello, banana!'
 // - hello('DETROIT') should return 'Hello, DETROIT!'
 
-
+function hello(name) { 
+    return "Hello, " + name + "!";
+  }
+  hello();
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+
+function helloDefault(name = "World") { 
+    return "Hello, " + name + "!";
+  }
+  hello();
